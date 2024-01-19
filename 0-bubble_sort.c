6ca56f08c6ad9f49c4ff26210d7e629ace2ef767
Moharum1 <moharum4@gamil.com>
2024-01-19 20:25:02 +0200

    task done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,19 +1,25 @@
 #include "sort.h"
 
+/**
+  * bubble_sort - A function for bubble sorting.
+  * @array: The array to sort.
+  * @size: THe length of the aaray.
+  * Return: Nothing.
+  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	bool isSorted;
 	int temp;
-	
+
 	if (array == NULL || size == 0)
 		return;
-	
+
 	for (i = 0; i <= (size - 1); i++)
 	{
 		isSorted = true;
-		
-		for (j =0; j <= (size - 2); j++)
+
+		for (j = 0; j <= (size - 2); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -25,7 +31,7 @@ void bubble_sort(int *array, size_t size)
 				isSorted = false;
 			}
 		}
-		
+
 		if (isSorted)
 			return;
 	}
