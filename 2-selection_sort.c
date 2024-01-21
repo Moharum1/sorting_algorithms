86f47fef4177720162404b3e3a0f8a14d605d659
Moharum1 <moharum4@gamil.com>
2024-01-21 20:31:46 +0200

    task done

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, smallest;
+    int temp;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		smallest = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[smallest])
+				smallest = j;
+		}
+
+		temp = array[smallest];
+		array[smallest] = array[i];
+		array[i] = temp;
+
+		print_array(array, size);
+	}
+
+}
