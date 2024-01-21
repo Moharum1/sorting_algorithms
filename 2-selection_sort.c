864f6add4cf7a1b0779fb416ef096fe7a52e4d1c
Moharum1 <moharum4@gamil.com>
2024-01-21 20:36:17 +0200

    task done

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,7 +5,7 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, smallest;
     int temp;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
