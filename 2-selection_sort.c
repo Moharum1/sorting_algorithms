166dd6a52d89f624deb08f1186c5171384a25cb5
Moharum1 <moharum4@gamil.com>
2024-01-21 20:37:54 +0200

    task done

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,9 +1,17 @@
 #include "sort.h"
 
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, smallest;
-    int temp;
+	int temp;
 
 	if (array == NULL || size < 2)
 		return;
